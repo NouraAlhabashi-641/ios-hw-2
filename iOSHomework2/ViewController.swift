083daa3ff51d1792c1fb04be3989126ae6971afb
n0bxii <kimnoura8@gmail.com>
2020-06-23 18:04:39 +0300

    Done

--- iOSHomework2/ViewController.swift
+++ iOSHomework2/ViewController.swift
@@ -20,11 +20,12 @@ class ViewController: UIViewController {
         
         // MARK: -   1️⃣ تحت الخط membersNamesArray إلي المصفوفه memberقم بإضافة المتغير
         
-        
+        membersNamesArray.append(member)
         
         // MARK: -   النهاية
         
         nameTextField.text = ""
+       
     }
     
     
@@ -34,7 +35,7 @@ class ViewController: UIViewController {
         
         // MARK: -  4️⃣ functionCall داخل المتغير  secretNameLetter قم باستدعاء الدالة
         
-        var functionCall = ""
+        var functionCall = secretNameLetter(memberNameArry: membersNamesArray)
         
         
         // MARK: -   النهاية
@@ -52,8 +53,7 @@ class ViewController: UIViewController {
         
         // MARK: -  5️⃣ functionCall داخل المتغير  secretNameEmoji قم باستدعاء الدالة
         
-        var functionCall = ""
-        
+var functionCall = secretNameEmoji(membersNameArry: membersNamesArray)
         // MARK: -   النهاية
         
         secretSocietyNameLabel.text =  functionCall
@@ -65,20 +65,50 @@ class ViewController: UIViewController {
     
     
     // MARK: - 2️⃣ تحت هذا الخط secretNameLetter قم بكتابة الداله
-    
-    
+    func secretNameLetter(memberNameArry :[String]) -> String{
+        var firsrtLetterArray: [String] = []
+        for memberName in membersNamesArray {
+            firsrtLetterArray.append(String (memberName.prefix(1)))
+            print(firsrtLetterArray.joined())
+        }
+        membersNamesArray.removeAll()
+        
+        return firsrtLetterArray.joined().uppercased()
+    }
     // MARK: -   النهاية
+    func secretNameEmoji(membersNameArry : [String]) -> String {
+     let emojiDicionaryEnglish = [ "A": "🥰", "B": "😳", "C": "🤯", "D": "🥺", "E": "🐶", "F": "🐣", "G": "🐧", "H": "❌", "I": "🐋", "J": "🌈", "K": "🍟", "L": "🌯", "M": "🍔", "N": "🍕", "O": "🍭", "P": "🧃", "Q": "🏸", "R": "🎹", "S": "📸", "T": "💸", "U": "📝", "V": "🤍", "W": "💤", "X": "Ⓜ️", "Y": "🃏", "Z": "🌭" ]
+     var emojiLetterArray: [String] = []
+     for i in membersNameArry {
+         let firstLetter = String(i.prefix(1))
+         for (letter,emoji) in emojiDicionaryEnglish {
+             if letter == firstLetter {
+                 emojiLetterArray.append(emoji)
+                 print(emojiLetterArray.joined())
+             }
+         }
+     
+     }
     
-    
-    
+
+    return emojiLetterArray.joined()
+         
+        }
     
     
     // MARK: - 3️⃣ تحت هذا الخط secretNameEmoji قم بكتابة الداله
+
     
     
+
     // MARK: -   النهاية
     
     
     
+
+
+
+
+
 }
 
